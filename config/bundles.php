acf4a3eb5260d5f51fa13d4ed9ff233fff1b4738
MZabolotny <mzabolotnyi@gmail.com>
2019-03-14 17:18:41 +0200

    add fos oauth and user

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,7 @@ return [
     FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
