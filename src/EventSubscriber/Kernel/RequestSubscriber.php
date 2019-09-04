0d7bb8309ec82252ff84535e9eac13e3cdd62351
MZabolotny <mzabolotnyi@gmail.com>
2019-09-04 17:15:37 +0300

    remove overhead listener

--- src/EventSubscriber/Kernel/RequestSubscriber.php
+++ src/EventSubscriber/Kernel/RequestSubscriber.php
@@ -2,13 +2,10 @@
 
 namespace App\EventSubscriber\Kernel;
 
-use Gedmo\Blameable\BlameableListener;
-use Gedmo\Loggable\LoggableListener;
 use Gedmo\Translatable\TranslatableListener;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RequestSubscriber implements EventSubscriberInterface
@@ -19,36 +16,20 @@ class RequestSubscriber implements EventSubscriberInterface
     /** @var TranslatorInterface */
     private $translator;
 
-    /** @var TokenStorageInterface */
-    private $tokenStorage;
-
-    /** @var LoggableListener */
-    private $loggableListener;
-
-    /** @var BlameableListener */
-    private $blameableListener;
-
 
     public function __construct(
         TranslatableListener $translatableListener,
-        TranslatorInterface $translator,
-        TokenStorageInterface $tokenStorage,
-        LoggableListener $loggableListener,
-        BlameableListener $blameableListener
+        TranslatorInterface $translator
     )
     {
         $this->translatableListener = $translatableListener;
         $this->translator = $translator;
-        $this->tokenStorage = $tokenStorage;
-        $this->loggableListener = $loggableListener;
-        $this->blameableListener = $blameableListener;
     }
 
     public static function getSubscribedEvents()
     {
         return [
             KernelEvents::REQUEST => [
-                ['handleUser', 0],
                 ['handleLocale', -1]
             ],
         ];
@@ -62,19 +43,5 @@ class RequestSubscriber implements EventSubscriberInterface
         $request->setLocale($locale);
         $this->translatableListener->setTranslatableLocale($locale);
         $this->translator->setLocale($locale);
-    }
-
-    public function handleUser(GetResponseEvent $event)
-    {
-        $blameUser = 'anonymous';
-
-        if (null !== $token = $this->tokenStorage->getToken()) {
-            if (\is_object($user = $token->getUser())) {
-                $blameUser = (string)$user->getId();
-            }
-        }
-
-        $this->loggableListener->setUsername($blameUser);
-        $this->blameableListener->setUserValue($blameUser);
     }
 }
